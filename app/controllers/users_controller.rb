bb682cbd2ef421d6bcbc02eb637237af4d6027b4
daiki-kanai <knidik3@gmail.com>
2020-04-30 16:11:19 +0900

    [settingLinkToEditUserInfomation]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @username = user.username
-    @address = user.address
-    @introduction = user.introduction
+    @user = User.find(params[:id])
   end
 end
