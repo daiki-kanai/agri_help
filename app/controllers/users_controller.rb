cd26895635635965aa5566fef05f73e0051a9a3d
daiki-kanai <knidik3@gmail.com>
2020-04-30 15:48:48 +0900

    [WIP]addIntroductionFormInSignUpView

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @username = user.username
+    @address = user.address
+    @introduction = user.introduction
   end
 end
