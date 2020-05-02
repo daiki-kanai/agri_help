03fe61866b7e6162c0d0c96c502b0eccfc4d480c
daiki-kanai <knidik3@gmail.com>
2020-05-02 18:26:22 +0900

    [WIP]settingUserIdInMessagesTable

--- db/migrate/20200501034305_create_messages.rb
+++ db/migrate/20200501034305_create_messages.rb
@@ -3,6 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.string :content,   null: false
       t.string :image
+      t.references :user, foreign_key: true
+
       t.timestamps
     end
   end
