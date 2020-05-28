27f9cc2975f79fc177ab865d4d925aed91761b0e
daiki-kanai <knidik3@gmail.com>
2020-05-29 00:25:53 +0900

    settingMessagesController

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,20 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!, only: :create
 
-  def index
-    @messages = Message.all
-    @message = Message.new
-  end
+  # def index
+  #   @messages = Message.all
+  #   @message = Message.new
+  # end
 
   def create
+    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
     Message.create(message_params)
+    redirect_to "/rooms/#{@message.room_id}"
   end
 
   private
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:user_id, :content, :image, :room_id).merge(user_id: current_user.id)
   end
 
 end
