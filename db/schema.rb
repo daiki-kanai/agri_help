53df2b5c91c7dd60de26d3b9af24394f9517ecb6
daiki-kanai <knidik3@gmail.com>
2020-05-28 23:06:47 +0900

    addRoomIdForeignKeyInMessagesTable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_133151) do
+ActiveRecord::Schema.define(version: 2020_05_28_140205) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_05_28_133151) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "room_id"
+    t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -44,5 +46,6 @@ ActiveRecord::Schema.define(version: 2020_05_28_133151) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
 end
