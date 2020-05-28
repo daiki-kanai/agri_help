423131bf14a2f3062ec3193d48c06d9fdf32ea87
daiki-kanai <knidik3@gmail.com>
2020-05-29 00:30:48 +0900

    fixTypeMiss

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @currentUserEntry = Entry.where(user_id: current_user.id)
-    @userEntry = Enty¥ry.where(user_id: @user.id)
+    @userEntry = Entry.where(user_id: @user.id)
 
     if @user.id = current_user.id
     else
