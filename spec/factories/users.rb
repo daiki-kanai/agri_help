cdfd4c8506b83324ce8d5e4c7410ac81c4aa5367
daiki-kanai <knidik3@gmail.com>
2020-06-06 22:21:20 +0900

    addModelTest

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     username              {"test"}
     email                 {"test@gmail.com"}
     address               {"東京都渋谷区"}
+    introduction          {"よろしくお願いします。"}
     password              {"000000"}
     password_confirmation {"000000"}
   end
