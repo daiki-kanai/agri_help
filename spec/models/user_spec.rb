0035f59218e8eaa417d93d8b8ffdfe16cbff7c98
daiki-kanai <knidik3@gmail.com>
2020-06-06 13:51:01 +0900

    useFactorybotInUserModelTest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     it "usernameがない場合は登録できないこと" do
-      user = User.new(username: "", email: "kkk@gmail.com", password: "000000", address: "東京都渋谷区", password_confirmation: "000000")
+      user = build(:user, username: "")
       user.valid?
       expect(user.errors[:username]).to include("を入力してください")
     end
