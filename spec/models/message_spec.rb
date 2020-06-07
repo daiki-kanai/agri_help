ad16f870de52fffafea2646e80e528641e495872
daiki-kanai <knidik3@gmail.com>
2020-06-07 17:38:56 +0900

    createMessageModelTest

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,9 +3,18 @@ describe Message do
   describe '#create' do
     it "contentとimageがあれば登録できる" do
       message = build(:message)
-      user = build(:user)
-      room = build(:room)
       expect(message).to be_valid
+    end
+
+    it "imageがなくてもcontentがあれば登録できる" do
+      message = build(:message, image: "")
+      expect(message).to be_valid
+    end
+
+    it "contentがなければ登録できない" do
+      message = build(:message, content: "")
+      message.valid?
+      expect(message.errors[:content]).to include("を入力してください")
     end
   end
 end
