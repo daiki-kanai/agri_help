f6e15b38d6b4f1edffcc473b719b62f358b65d4b
daiki-kanai <knidik3@gmail.com>
2020-06-08 20:50:35 +0900

    fixMessageModelTestCode

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :room do
-    id  {"100"}
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 describe Message do
   describe '#create' do
-    it "contentとimageがあれば登録できる" do
-      message = build(:message)
+    it "contentとimageがあれば投稿できる" do
+      user = create(:user)
+      room = create(:room)
+      message = build(:message, user_id: user.id, room_id: room.id)
       expect(message).to be_valid
     end
 
     it "imageがなくてもcontentがあれば登録できる" do
-      message = build(:message, image: "")
+      user = create(:user)
+      room = create(:room)
+      message = build(:message, image: "", user_id: user.id, room_id: room.id)
       expect(message).to be_valid
     end
 
     it "contentがなければ登録できない" do
-      message = build(:message, content: "")
+      user = create(:user)
+      room = create(:room)
+      message = build(:message, content: "", user_id: user.id, room_id: room.id)
       message.valid?
       expect(message.errors[:content]).to include("を入力してください")
     end
